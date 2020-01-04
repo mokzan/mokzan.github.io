2c3f622f71dd76e1c73d2e9286916b35f5e3b1de
Moussa Okzan <38858344+mokzan@users.noreply.github.com>
2020-01-04 16:51:17 +0100

    Add files via upload

---
+++ ccm-counter.js
@@ -0,0 +1,36 @@
+(() => {
+    const component = {
+      name: 'counter-mo',
+      ccm: 'https://ccmjs.github.io/ccm/ccm.js',
+      config: {
+        css: ['ccm.load', 'counter.css'],
+        counter: 0      
+      },
+      Instance: function () {
+        const inc = document.createElement('button');
+        const dec = document.createElement('button');
+        inc.innerText = 'Inc';
+        inc.onclick = () => {
+          this.counter++;
+          this.start()
+        };
+        dec.innerText = 'Dec';
+        dec.onclick = () => {
+          this.counter--;
+          this.start();
+        }
+        this.init = async () => {
+          
+          
+        }
+        this.start = async () => {
+          
+          this.element.innerHTML = `<div>${this.counter}</div>`
+          this.element.appendChild(inc)
+          this.element.appendChild(dec)
+        }
+      }
+    }
+  
+    let b = "ccm." + component.name + (component.version ? "-" + component.version.join(".") : "") + ".js"; if (window.ccm && null === window.ccm.files[b]) return window.ccm.files[b] = component; (b = window.ccm && window.ccm.components[component.name]) && b.ccm && (component.ccm = b.ccm); "string" === typeof component.ccm && (component.ccm = { url: component.ccm }); let c = (component.ccm.url.match(/(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)/) || ["latest"])[0]; if (window.ccm && window.ccm[c]) window.ccm[c].component(component); else { var a = document.createElement("script"); document.head.appendChild(a); component.ccm.integrity && a.setAttribute("integrity", component.ccm.integrity); component.ccm.crossorigin && a.setAttribute("crossorigin", component.ccm.crossorigin); a.onload = function () { window.ccm[c].component(component); document.head.removeChild(a) }; a.src = component.ccm.url }
+  })();
